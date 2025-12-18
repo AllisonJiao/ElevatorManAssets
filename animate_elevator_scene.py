2e79c12e95b91d1dcd137b82dedae14cdf0367f5
AllisonJiao <112785687+AllisonJiao@users.noreply.github.com>
2025-12-18 12:36:02 -0800

    IK animate the robot

--- animate_elevator_scene.py
+++ animate_elevator_scene.py
@@ -11,6 +11,7 @@ from isaaclab.app import AppLauncher
 parser = argparse.ArgumentParser(
     description="This script demonstrates adding a custom elevator to an Isaac Lab environment."
 )
+parser.add_argument("--robot", type=str, default="agibot", help="Name of the robot.")
 parser.add_argument("--num_envs", type=int, default=1, help="Number of environments to spawn.")
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
@@ -25,44 +26,105 @@ import torch
 
 import isaaclab.sim as sim_utils
 # import prims as prim_utils
-from isaaclab.assets import Articulation
-from isaaclab.sim import SimulationContext
+from isaaclab.assets import AssetBaseCfg
+from isaaclab.controllers import DifferentialIKController, DifferentialIKControllerCfg
+from isaaclab.managers import SceneEntityCfg
+from isaaclab.markers import VisualizationMarkers
+from isaaclab.markers.config import FRAME_MARKER_CFG
+from isaaclab.scene import InteractiveScene, InteractiveSceneCfg
+from isaaclab.utils import configclass
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+from isaaclab.utils.math import subtract_frame_transforms
 
-from cfg.agibot import AGIBOT_A2D_CFG
+##
+# Pre-defined configs
+##
+from cfg.agibot import AGIBOT_A2D_CFG  # isort:skip
 from cfg.elevator import ELEVATOR_CFG
 
 # USD access
 import omni.usd
 from pxr import UsdGeom, Gf, Usd
 
-def design_scene() -> tuple[dict]:
-    """Designs the scene."""
+ELEVATOR_ASSET_PATH = "ElevatorManAssets/assets/Collected_elevator_asset_tmp/elevator_asset.usdc"
 
-    # Ground-plane
-    cfg = sim_utils.GroundPlaneCfg()
-    cfg.func("/World/defaultGroundPlane", cfg)
+@configclass
+class ElevatorSceneCfg(InteractiveSceneCfg):
+    """Configuration for an elevator man scene."""
 
-    # Light(s)
-    cfg = sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
-    cfg.func("/World/Light", cfg)
+    # ground plane
+    ground = AssetBaseCfg(
+        prim_path="/World/defaultGroundPlane",
+        spawn=sim_utils.GroundPlaneCfg(),
+        init_state=AssetBaseCfg.InitialStateCfg(pos=(0.0, 0.0, -1.05)),
+    )
 
-    # Elevator
-    elevator_cfg = ELEVATOR_CFG.copy()
-    elevator_cfg.prim_path = "/World/Elevator/root"
-    elevator = Articulation(cfg = elevator_cfg)
+    # lights
+    dome_light = AssetBaseCfg(
+        prim_path="/World/Light", spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
+    )
 
-    # Robot(s)
-    agibot_cfg = AGIBOT_A2D_CFG.copy()
-    agibot_cfg.prim_path = "/World/Robot"
-    agibot = Articulation(cfg = agibot_cfg)
+    # elevator
+    elevator = AssetBaseCfg(
+        prim_path="/World/Elevator/root",
+        spawn=sim_utils.UsdFileCfg(
+            usd_path=f"{ELEVATOR_ASSET_PATH}", scale=(1.0, 1.0, 1.0)
+        ),
+    )
 
-    scene_entities = {"agibot": agibot, "elevator": elevator}
-    return scene_entities
+    # articulation
+    if args_cli.robot == "agibot":
+        robot = AGIBOT_A2D_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+    else:
+        raise ValueError(f"Robot {args_cli.robot} is not supported. Valid: agibot")
 
+def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
+    """Runs the simulation loop."""
+    # Extract scene entities
+    # note: we only do this here for readability.
+    robot = scene["robot"]
 
-def run_simulator(sim: sim_utils.SimulationContext, entities: dict[str, Articulation]):
-    agibot = entities["agibot"]
-    elevator = entities["elevator"]
+    # Create controller
+    diff_ik_cfg = DifferentialIKControllerCfg(command_type="pose", use_relative_mode=False, ik_method="dls")
+    diff_ik_controller = DifferentialIKController(diff_ik_cfg, num_envs=scene.num_envs, device=sim.device)
+
+    # Markers
+    frame_marker_cfg = FRAME_MARKER_CFG.copy()
+    frame_marker_cfg.markers["frame"].scale = (0.1, 0.1, 0.1)
+    ee_marker = VisualizationMarkers(frame_marker_cfg.replace(prim_path="/Visuals/ee_current"))
+    goal_marker = VisualizationMarkers(frame_marker_cfg.replace(prim_path="/Visuals/ee_goal"))
+
+    # Define goals for the arm
+    ee_goals = [
+        [0.5, 0.5, 0.7, 0.707, 0, 0.707, 0],
+        [0.5, -0.4, 0.6, 0.707, 0.707, 0.0, 0.0],
+        [0.5, 0, 0.5, 0.0, 1.0, 0.0, 0.0],
+    ]
+    ee_goals = torch.tensor(ee_goals, device=sim.device)
+    # Track the given command
+    current_goal_idx = 0
+    # Create buffers to store actions
+    ik_commands = torch.zeros(scene.num_envs, diff_ik_controller.action_dim, device=robot.device)
+    ik_commands[:] = ee_goals[current_goal_idx]
+
+    # Specify robot-specific parameters
+    if args_cli.robot == "agibot":
+        robot_entity_cfg = SceneEntityCfg(
+            "robot", 
+            joint_names=["left_arm_joint[1-7]"], 
+            body_names=["Link6_l"]
+        )
+    else:
+        raise ValueError(f"Robot {args_cli.robot} is not supported. Valid: agibot, ur10")
+    # Resolving the scene entities
+    robot_entity_cfg.resolve(scene)
+    # Obtain the frame index of the end-effector
+    # For a fixed base robot, the frame index is one less than the body index. This is because
+    # the root body is not included in the returned Jacobians.
+    if robot.is_fixed_base:
+        ee_jacobi_idx = robot_entity_cfg.body_ids[0] - 1
+    else:
+        ee_jacobi_idx = robot_entity_cfg.body_ids[0]
 
     # --- Door2 prim transform animation (mesh-only) ---
     DOOR2_PRIM_PATH = "/World/Elevator/root/Elevator/ElevatorRig/Door2/Cube_025"
@@ -81,14 +143,7 @@ def run_simulator(sim: sim_utils.SimulationContext, entities: dict[str, Articula
     init_t = Gf.Vec3d(init_t)
     print("[INFO] Door2 initial translate:", init_t)
 
-    # --- Agibot joint animation setup ---
-    animate_agibot_joint_names = [
-        n for n in agibot.data.joint_names
-        if n.startswith("left_arm_joint") or n.startswith("right_arm_joint")
-    ]
-    animate_agibot_ids, _ = agibot.find_joints(animate_agibot_joint_names)
-    animate_agibot_ids = torch.as_tensor(animate_agibot_ids, device=agibot.device, dtype=torch.long)
-
+    # Define simulation stepping
     sim_dt = sim.get_physics_dt()
     count = 0
     period = 500
@@ -99,65 +154,81 @@ def run_simulator(sim: sim_utils.SimulationContext, entities: dict[str, Articula
     while simulation_app.is_running():
         if count % period == 0:
             count = 0
-            for a in [agibot]:
-                root_state = a.data.default_root_state.clone()
-                a.write_root_pose_to_sim(root_state[:, :7])
-                a.write_root_velocity_to_sim(root_state[:, 7:])
-
-                joint_pos = a.data.default_joint_pos.clone()
-                joint_vel = a.data.default_joint_vel.clone()
-                joint_pos += torch.rand_like(joint_pos) * 0.1 # small noise
-                a.write_joint_state_to_sim(joint_pos, joint_vel)
-                a.reset()
+            # reset joint state
+            joint_pos = robot.data.default_joint_pos.clone()
+            joint_vel = robot.data.default_joint_vel.clone()
+            robot.write_joint_state_to_sim(joint_pos, joint_vel)
+            robot.reset()
+            # reset actions
+            ik_commands[:] = ee_goals[current_goal_idx]
+            joint_pos_des = joint_pos[:, robot_entity_cfg.joint_ids].clone()
+            # reset controller
+            diff_ik_controller.reset()
+            diff_ik_controller.set_command(ik_commands)
+            # change goal
+            current_goal_idx = (current_goal_idx + 1) % len(ee_goals)
             print("[INFO]: Resetting state...")
+        else:
 
-        alpha = (count % period) / max(1, period - 1)
+            phase = count % period
+            if phase < 100:        # opening
+                t = phase / 99.0
+                delta = close_delta + t * (open_delta - close_delta)
+            elif phase < 400:      # hold open
+                delta = open_delta
+            else:                  # closing
+                t = (phase - 400) / 99.0
+                delta = open_delta + t * (close_delta - open_delta)
 
-        phase = count % period
-        if phase < 100:        # opening
-            t = phase / 99.0
-            delta = close_delta + t * (open_delta - close_delta)
-        elif phase < 400:      # hold open
-            delta = open_delta
-        else:                  # closing
-            t = (phase - 400) / 99.0
-            delta = open_delta + t * (close_delta - open_delta)
+            # control door2 mesh transform
+            new_t = Gf.Vec3d(init_t[0] + delta, init_t[1], init_t[2])
+            door2_xform.SetTranslate(new_t)
 
-        # control door2 mesh transform
-        new_t = Gf.Vec3d(init_t[0] + delta, init_t[1], init_t[2])
-        door2_xform.SetTranslate(new_t)
-
-        # control agibot
-        joint_pos_target = agibot.data.default_joint_pos.clone()
-        joint_pos_target[:, animate_agibot_ids] += alpha * (2 * torch.pi)
-        joint_pos_target = joint_pos_target.clamp_(
-            agibot.data.soft_joint_pos_limits[..., 0], agibot.data.soft_joint_pos_limits[..., 1]
-        )
-        agibot.set_joint_position_target(joint_pos_target)
-
-        # write to sim
-        agibot.write_data_to_sim()
-
+            # control agibot
+            # obtain quantities from simulation
+            jacobian = robot.root_physx_view.get_jacobians()[:, ee_jacobi_idx, :, robot_entity_cfg.joint_ids]
+            ee_pose_w = robot.data.body_pose_w[:, robot_entity_cfg.body_ids[0]]
+            root_pose_w = robot.data.root_pose_w
+            joint_pos = robot.data.joint_pos[:, robot_entity_cfg.joint_ids]
+            # compute frame in root frame
+            ee_pos_b, ee_quat_b = subtract_frame_transforms(
+                root_pose_w[:, 0:3], root_pose_w[:, 3:7], ee_pose_w[:, 0:3], ee_pose_w[:, 3:7]
+            )
+            # compute the joint commands
+            joint_pos_des = diff_ik_controller.compute(ee_pos_b, ee_quat_b, jacobian, joint_pos)
+        
+        # apply actions
+        robot.set_joint_position_target(joint_pos_des, joint_ids=robot_entity_cfg.joint_ids)
+        scene.write_data_to_sim()
+        # perform step
         sim.step()
+        # update sim-time
         count += 1
+        # update buffers
+        scene.update(sim_dt)
 
-        agibot.update(sim_dt)
+        # obtain quantities from simulation
+        ee_pose_w = robot.data.body_state_w[:, robot_entity_cfg.body_ids[0], 0:7]
+        # update marker positions
+        ee_marker.visualize(ee_pose_w[:, 0:3], ee_pose_w[:, 3:7])
+        goal_marker.visualize(ik_commands[:, 0:3] + scene.env_origins, ik_commands[:, 3:7])
 
 def main():
     """Main function."""
     # Load kit helper
     sim_cfg = sim_utils.SimulationCfg(device=args_cli.device)
-    sim = SimulationContext(sim_cfg)
+    sim = sim_utils.SimulationContext(sim_cfg)
     # Set main camera
     sim.set_camera_view([2.5, 0.0, 4.0], [0.0, 0.0, 2.0])
     # Design scene
-    scene_entities = design_scene()
+    scene_cfg = ElevatorSceneCfg(num_envs=args_cli.num_envs, env_spacing=2.0)
+    scene = InteractiveScene(scene_cfg)
     # Play the simulator
     sim.reset()
     # Now we are ready!
     print("[INFO]: Setup complete...")
     # Run the simulator
-    run_simulator(sim, scene_entities)
+    run_simulator(sim, scene)
 
 
 if __name__ == "__main__":
